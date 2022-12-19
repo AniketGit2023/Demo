7a05d44a8fa41e439597c0f2e7adff8f419dfd95
AniketGit2023 <aniketkarpe2023@gmail.com>
2022-12-19 22:37:00 +0530

    Substractiom succesfully

--- ForDemo/src/com/Sample/Addition.java
+++ ForDemo/src/com/Sample/Addition.java
@@ -4,7 +4,7 @@ public class Addition {
 	public static void main(String[] args) {
 		int a=10,b=20;
 		System.out.println("Addition is:"+(a+b));
-		
+		System.out.println("Substraction is:"+(b-a));
 	}
 
 }

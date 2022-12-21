84b5b7458459e495e61aef08d448a3a41b9aebb0
AniketGit2023 <aniketkarpe2023@gmail.com>
2022-12-21 10:17:43 +0530

    Arraylist added SucessFully

---
+++ ForDemo/src/com/Sample/Arraylist.java
@@ -0,0 +1,19 @@
+package com.Sample;
+
+import java.util.ArrayList;
+
+public class Arraylist {
+
+	public static void main(String[] args) {
+		ArrayList al=new ArrayList();
+		al.add(30);
+		al.add(40);
+		al.add(20);
+		int sum=0;
+		for(Object obj:al)
+		{
+			
+		}
+	}
+
+}
